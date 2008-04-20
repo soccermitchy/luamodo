e1d0afea27c0a9cd2c4e845e328df218f3c202a3
nckomodo <nckomodo@95f28da3-554b-0410-8188-9bf4f2bc0f13>
2008-04-20 23:58:24 +0000

    Removed file/folder

--- trunk/ luamodo --username nckomodo/Bots/TestBot.lua
+++
@@ -1,5 +0,0 @@
-TestBot = Bot.New("Test Bot",{"TestBot","TestBot2","TestBot3"}) --the first argument is what name to use when displaying informationg through the console, the second argument is a table of nicks to use for connections
-TestBot:Connect("IRC.CHANGETHISSERVER.NET", 6667, {{channel = "#somechannel"}, {channel = "#somepasswordedchannel", password = "somepassword"}}) -- note there is another optional argument at the end, which is the command to send to the server to authenticate (such as "nickerv identify somepassword")
-TestBot:LoadExtension(Stats)
-TestBot:LoadExtension(Control)
-TestBot:AddCTCP("VERSION", function(Func,Args) Args.Connection:CTCP(Args.Nick, "VERSION Some sort of version reply") end)
